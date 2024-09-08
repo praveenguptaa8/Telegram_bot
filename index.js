66352390967b21e14bac01d2f3fda16c05f5d99c
Praveen Gupta <praveenguptaa8@gmail.com>
2024-09-08 14:56:40 +0530

    Create bot

---
+++ index.js
@@ -0,0 +1,29 @@
+const { Telegraf } = require('telegraf');
+
+const bot = new Telegraf('7364826423:AAHNcqdPlDrSqZlkQ3tTjCg9ZA43ho6pQ2U');
+
+bot.start((ctx) => ctx.reply(`Welcome !
+Type /help for commands
+Or Try sending a sticker
+     `));
+bot.help((ctx) => ctx.reply(`
+    Use commands:
+    /start : To start the bot
+    /help : To know the commands
+    /greet : To get greeting
+
+    
+
+    `))
+bot.command('greet', (ctx) => {
+    ctx.reply('Namaste Sir');
+})
+
+
+bot.on(('sticker'), (ctx) => ctx.reply('👍'))
+
+
+
+
+
+bot.launch();
